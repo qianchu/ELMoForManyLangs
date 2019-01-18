6d12dbab1d954b1a5d5231918a146ea6058754e8
qianchu <hey_flora@126.com>
2019-01-18 20:16:25 +0000

    batchsize

--- elmo2word2vec.sh
+++ elmo2word2vec.sh
@@ -4,6 +4,7 @@ ch_vocab=$1 # absolute path for sentence files
 model_path=$2
 output_layer=$3
 gpu=$4
+batch=$5
 
 
 # conll vocab
@@ -12,7 +13,7 @@ python ../../corpora/convert_vocab2conll.py $ch_vocab $ch_vocab'.conll'
 # produce elmo representation
 output_prefix_ch=$model_path$(basename $ch_vocab)
 
-python -m elmoformanylangs test     --input_format conll  --gpu $4   --input $ch_vocab'.conll'     --model $model_path    --output_prefix $model_path$(basename $output_prefix_ch)   --output_layer $output_layer   --output_format hdf5
+python -m elmoformanylangs test   --batch $batch  --input_format conll  --gpu $gpu   --input $ch_vocab'.conll'     --model $model_path    --output_prefix $model_path$(basename $output_prefix_ch)   --output_layer $output_layer   --output_format hdf5
 echo 'output file: '$output_prefix_ch'.ly'$output_layer'.hdf5'
 
 # convert to word2vec
