5c80407ed575652758ff2659d5a26f7fc852ead3
qianchu <hey_flora@126.com>
2019-02-08 07:12:16 +0800

    vocab word2vec

--- h5py2word2vec.py
+++ h5py2word2vec.py
@@ -7,7 +7,10 @@ def h5py2word2vec(h5py_file,output_f):
         output_f.write('{0} {1}\n'.format(str(len(f.keys())),len(f[list(f.keys())[0]][0])))
         for sent in f.keys():
             for i, word in enumerate(sent.split('\t')):
-                output_f.write('{0}||{1}'.format(sent,i)+' '+' '.join([str(v) for v in f[sent][i]])+'\n')
+                if len(sent.split('\t'))==1:
+                    output_f.write('{0}'.format(sent)+' '+' '.join([str(v) for v in f[sent][i]])+'\n')
+                else:
+                    output_f.write('{0}||{1}'.format(sent,i)+' '+' '.join([str(v) for v in f[sent][i]])+'\n')
 
 if __name__=='__main__':
     import sys
